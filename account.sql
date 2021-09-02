14369565cf33b778699fb082a19bc65406eead98
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-09-02 16:46:36 +0700

    [add] insert data

---
+++ account.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM projector.account
+LIMIT 0, 1000
+
+-- Date: 2021-09-02 16:10
+*/
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accRole`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`) VALUES (1,'yamjaii','jaiyen123','member','yamcha','fainting','0833333333','88 north kaio\'s planet 86868',1);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accRole`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`) VALUES (2,'sunshinewink','moonsun','member','arthit','songsaeng','0822222222','99 land-ofthesun99/9 sawang 12345 ',2);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accRole`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`) VALUES (3,'somdui','12345','admin','somdui','puipui','0811111111','112 inyourheart22/2 forever 12222',3);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accRole`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`) VALUES (4,'kiritokun','asuna','admin','kirito','kirigaya','0812345689','36 swordland10 underworld 11111',4);
---
+++ cart.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM projector.cart
+LIMIT 0, 1000
+
+-- Date: 2021-09-02 16:11
+*/
+INSERT INTO `cart` (`idCart`) VALUES (1);
+INSERT INTO `cart` (`idCart`) VALUES (2);
+INSERT INTO `cart` (`idCart`) VALUES (3);
+INSERT INTO `cart` (`idCart`) VALUES (4);
