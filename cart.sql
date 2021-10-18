6990b8a93060294e2c8c493107944824c935c70c
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-10-18 15:52:34 +0700

    [add] insert totalPrice

--- cart.sql
+++ cart.sql
@@ -4,7 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2021-09-02 16:11
 */
-INSERT INTO `cart` (`idCart`) VALUES (1);
-INSERT INTO `cart` (`idCart`) VALUES (2);
-INSERT INTO `cart` (`idCart`) VALUES (3);
-INSERT INTO `cart` (`idCart`) VALUES (4);
+INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (1, 0);
+INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (2, 0);
+INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (3, 0);
+INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (4, 0);
