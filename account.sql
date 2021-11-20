5da318d7ceca945676921f7a7f762b65f7323a8b
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-11-20 22:38:35 +0700

    [edit] encrypt accPass in account

--- account.sql
+++ account.sql
@@ -4,7 +4,6 @@ LIMIT 0, 1000
 
 -- Date: 2021-09-02 16:10
 */
-INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (1,'yamjaii','jaiyen123','yamcha','fainting','0833333333','88 north kaio\'s planet 86868',1,3);
-INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (2,'sunshinewink','moonsun','arthit','songsaeng','0822222222','99 land-ofthesun99/9 sawang 12345 ',2,3);
-INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (3,'somdui','12345','somdui','puipui','0811111111','112 inyourheart22/2 forever 12222',3,1);
-INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (4,'kiritokun','asuna','kirito','kirigaya','0812345689','36 swordland10 underworld 11111',4,1);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (1,'aihara','$2a$12$Km5.X9jeS2XTQHn/uZYY/eUEwoH4ER0DbYptVqbOC7ofd8EKx4bi.','aihara','mei','0833333333','88 north kaio\'s planet 86868',1,1);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (2,'asada','$2a$12$6u3xrT51Xa203E4OFbklHOuU.xlpF97guzc89/jI8sn4VQ29lV9Y6','asada','sinon','0822222222','99 land-ofthesun99/9 sawang 12345',2,2);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (3,'raiden','$2a$12$69e630EjMJ3wqTaJ/eR6DeH7RiCTJj./Cz7Q5iH/e5tP7WKnUkTiS','raiden','shogun','0811111111','112 inyourheart22/2 forever 12222',3,3);
