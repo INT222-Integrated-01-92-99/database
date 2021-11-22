e5140937ffaf7cbc37ddb35ed10ae7c3257022a4
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-11-22 15:40:05 +0700

    [edit] insert cart

--- cart.sql
+++ cart.sql
@@ -7,4 +7,3 @@ LIMIT 0, 1000
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (1, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (2, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (3, 0);
-INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (4, 0);
--- ppclothesDB.sql
+++ ppclothesDB.sql
@@ -239,7 +239,6 @@ USE `project`;
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (1, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (2, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (3, 0);
-INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (4, 0);
 
 COMMIT;
 
