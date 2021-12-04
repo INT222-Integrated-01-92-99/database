b9901f7f8cbde988085d645bcb81f88dcca9c277
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-12-04 13:58:06 +0700

    [edit] insert new account test

--- ppclothesDB.sql
+++ ppclothesDB.sql
@@ -320,6 +320,7 @@ LIMIT 0, 1000
 INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (1,'aihara','$2a$12$Km5.X9jeS2XTQHn/uZYY/eUEwoH4ER0DbYptVqbOC7ofd8EKx4bi.','aihara','mei','0833333333','88 north kaio\'s planet 86868',1,1);
 INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (2,'asada','$2a$12$6u3xrT51Xa203E4OFbklHOuU.xlpF97guzc89/jI8sn4VQ29lV9Y6','asada','sinon','0822222222','99 land-ofthesun99/9 sawang 12345',2,2);
 INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (3,'raiden','$2a$12$69e630EjMJ3wqTaJ/eR6DeH7RiCTJj./Cz7Q5iH/e5tP7WKnUkTiS','raiden','shogun','0811111111','112 inyourheart22/2 forever 12222',3,3);
+INSERT INTO `account` (`idAccount`,`accUsername`,`accPass`,`accFname`,`accLname`,`accPhone`,`accAddress`,`idCart`, `idRole`) VALUES (4,'test','$2a$12$m/7pR.HPNOU6XEQVFHEdBOsF2q5QnvqQL8ZeoAH6NNhY5ikpf0tlm','test','test','0844444444','test',4,3);
 COMMIT;
 -- -----------------------------------------------------
 -- Table `project`.`CartDetails`
