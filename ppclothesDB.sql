1ae1e0c82689f4ca401226f5f66c26b36a56a80b
Samita Pinijsupol <samita.pin@mail.kmutt.ac.th>
2021-12-04 14:02:58 +0700

    [edit] add new cart

--- ppclothesDB.sql
+++ ppclothesDB.sql
@@ -239,6 +239,7 @@ USE `project`;
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (1, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (2, 0);
 INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (3, 0);
+INSERT INTO `cart` (`idCart`, `totalPrice`) VALUES (4, 0);
 
 COMMIT;
 
